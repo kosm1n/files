595c15051e6ba415b3d3908ad5240a76bc0623c6
kosm1n <cc_cosmin@hotmail.com>
2020-04-13 20:33:17 +0200

    files first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,58 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.FileTime;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
+
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+//         File file = new File("G:/DCIM/Camera/Photo taken on 2011-08-23 19-08-08.jpg");
+//         setCreationTimeFromFileName(file);
+
+        File folder = new File("G:/DCIM/Camera/");
+        List<File> listOfFiles = Arrays.asList(folder.listFiles());
+
+        listOfFiles.forEach(Main::setCreationTimeFromFileName);
+
+    }
+
+    private static void setCreationTimeFromFileName(File file) {
+
+        try {
+
+            // Get File Attributes
+            BasicFileAttributeView basicFileAttributeView = Files.getFileAttributeView(file.toPath(), BasicFileAttributeView.class);
+
+            // Get Date from File Name
+            Date date = RawDate.getDateFromFileName(file.getName());
+
+            // Convert from Date to FileTime
+            FileTime time = FileTime.fromMillis(date.getTime());
+
+            // Set Modified, Acessed & Created Times
+            basicFileAttributeView.setTimes(time, time, time);
+
+            log.info("File: " + file.getName() + " new creation date: '{}'", basicFileAttributeView.readAttributes().creationTime());
+
+        } catch (ParseException e) {
+            log.error("Error parsing file name: " + file.getName(), e);
+        } catch (IOException e) {
+            log.error("Error setting file dates: " + file.getName(), e);
+        } catch (Exception e) {
+            log.error("Unknown error on file: " + file.getName(), e);
+        }
+
+    }
+
+}
---
+++ src/main/java/RawDate.java
@@ -0,0 +1,69 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.regex.Pattern;
+
+public enum RawDate {
+
+    SAMSUNG_IMG("[1-2][0-9][0-9][0-9][0-1][0-9][0-3][0-9](_)[0-2][0-9][0-5][0-9][0-5][0-9](.jpg)", "yyyyMMdd_HHmmss"),
+    SAMSUNG_VID("[1-2][0-9][0-9][0-9][0-1][0-9][0-3][0-9](_)[0-2][0-9][0-5][0-9][0-5][0-9](.mp4)", "yyyyMMdd_HHmmss"),
+    SAMSUNG_OLD("(Photo taken on )[1-2][0-9][0-9][0-9](-)[0-1][0-9](-)[0-3][0-9]( )[0-2][0-9](-)[0-5][0-9](-)[0-5][0-9](.jpg)", "yyyy-MM-dd HH-mm-ss"),
+    WHATSAPP_IMG("IMG-[1-2][0-9][0-9][0-9][0-1][0-9][0-3][0-9](-WA)[0-9]+(.jpg|.jpeg)", "yyyyMMdd"),
+    WHATSAPP_VID("VID-[1-2][0-9][0-9][0-9][0-1][0-9][0-3][0-9](-WA)[0-9]+(.mp4)", "yyyyMMdd");
+
+    private String pattern;
+    private String format;
+
+    RawDate(String pattern, String format) {
+        this.pattern = pattern;
+        this.format = format;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public static RawDate getDateFormatsEnum(String fileName) {
+        return Arrays.stream(RawDate.values())
+                .filter(value ->
+                        Pattern.compile(value.getPattern()).matcher(fileName).matches())
+                .findFirst().orElseThrow(() -> new RuntimeException("No date format found for the file name: " + fileName));
+    }
+
+    public static Date getDateFromFileName(String fileName) throws ParseException {
+        RawDate rawDate = getDateFormatsEnum(fileName);
+        switch (rawDate) {
+            case SAMSUNG_IMG:
+                return new SimpleDateFormat(rawDate.getFormat()).parse(fileName.replace(".jpg",""));
+            case SAMSUNG_VID:
+                return new SimpleDateFormat(rawDate.getFormat()).parse(fileName.replace(".mp4",""));
+            case SAMSUNG_OLD:
+                return new SimpleDateFormat(rawDate.getFormat())
+                        .parse(
+                                fileName.replaceFirst("(Photo taken on )","")
+                                        .replaceFirst("(.jpg)","")
+                        );
+            case WHATSAPP_IMG:
+                return new SimpleDateFormat(rawDate.getFormat())
+                        .parse(
+                                fileName.replace("IMG-","")
+                                        .replaceFirst("(.jpg|.jpeg)","")
+                                        .replaceFirst("(-WA)[0-9]+", "")
+                        );
+            case WHATSAPP_VID:
+                return new SimpleDateFormat(rawDate.getFormat())
+                        .parse(
+                                fileName.replace("VID-","")
+                                        .replace(".mp4","")
+                                        .replaceFirst("(-WA)[0-9]+", "")
+                );
+        }
+        return null;
+    }
+
+}
