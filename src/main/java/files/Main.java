46c70330b1224765baacba3df7eca261aadda589
kosm1n <cc_cosmin@hotmail.com>
2020-05-17 13:34:48 +0200

    move classes to package

--- src/main/java/files/Main.java
+++ src/main/java/files/Main.java
@@ -1,3 +1,5 @@
+package files;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- src/main/java/files/RawDate.java
+++ src/main/java/files/RawDate.java
@@ -1,3 +1,5 @@
+package files;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
